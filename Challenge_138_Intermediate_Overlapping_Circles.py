afd6765f7b99cdfcc602563a8b0b76a1f866eef6
Kyle Preston <kylepreston.122@gmail.com>
2014-01-26 00:57:02 -0800

    Create Challenge_138_Intermediate_Overlapping_Circles.py

---
+++ Challenge_138_Intermediate_Overlapping_Circles.py
@@ -0,0 +1,15 @@
+# My solution to /r/dailyprogrammer
+# Assuming unit circles
+
+from math import acos, pi, sqrt
+
+def circle_area(x1, y1, x2, y2):
+
+	# pythagorean theorem
+	dist = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+	# http://mathworld.wolfram.com/Circle-CircleIntersection.html
+	if dist >= 2:
+		return 2 * pi
+	else:
+		return (2 * pi) - ((2 * acos(dist/2)) + (dist/2) * sqrt(4 - dist ** 2))
