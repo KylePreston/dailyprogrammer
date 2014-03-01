9ebd2b630dd1dc5c8f68676b928b6bc08d510e3a
Kyle Preston <kylepreston.122@gmail.com>
2014-02-28 19:08:09 -0800

    Create Challenge_149_Easy_Disemvoweler.py

---
+++ Challenge_149_Easy_Disemvoweler.py
@@ -0,0 +1,18 @@
+vowel = 'aeiou'
+print "Press CTRL-Z to escape"
+def disemvoweler(sentence):
+	words = sentence.strip().split()
+	vowels = list()
+
+	for i in range(len(words)):
+		for char in words[i]:
+			if char in vowel:
+				vowels.append(char)
+				words[i] = words[i].replace(char, "")
+
+	return "".join(vowels), "".join(words)
+
+while True:
+	sent = raw_input("Enter your sentence: ")
+
+	print disemvoweler(sent)
