1e1dcc97067b459957ba03dabee14b1f616ca336
Kyle Preston <kylepreston.122@gmail.com>
2014-03-28 13:24:18 -0700

    Update Challenge_145_Easy_Tree_Generation.py

--- Challenge_145_Easy_Tree_Generation.py
+++ Challenge_145_Easy_Tree_Generation.py
@@ -5,7 +5,7 @@ def tree(height):
 	n = 1
 	space = height
 	for x in range(0, height):
-		print (" " * space + "@" * n)
+		print(" " * space + "@" * n)
 		n += 2; space -= 1
 
 ht = int(raw_input('How high should your tree be? '))
